0ef268d7ad1a64cc45d508e4d0b940a58bc9e487
nobodyw <michael.wong@epitech.eu>
2021-06-09 22:18:33 +0200

    add navbar style

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import './styles/navbar.css'
+import './navbar.js';
+
 // start the Stimulus application
 import './bootstrap';
 
